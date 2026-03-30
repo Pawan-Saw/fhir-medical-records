375c292821c60623e29fa01c87915646f215946f
Pawan Kumar <pksaw7717@gmail.com>
2026-03-30 18:37:21 +0530

    fix api base url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://fhir-api.up.railway.app',
 });
 
 // Har request mein token automatically add hoga
